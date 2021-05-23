2e541a224127bc55f9d470ec83a6e149c02282ef
Salahin <salahin.dsi@bkash.com>
2021-05-23 20:50:58 +0600

    state binding to text field

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,100 @@
 import {Component} from 'react';
+import {Button, Card, Col, Form} from 'react-bootstrap';
+
 
 export default class Book extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      author: '',
+      coverPhotoUrl: '',
+      isbnNumber: ''
+      
+    }
+    this.bookInfoChange = this.bookInfoChange.bind(this);
+    this.submitBook = this.submitBook.bind(this);
+  }
+  
+  submitBook(event) {
+    event.preventDefault();
+    alert('Title: ' + this.state.title +
+          'author: ' + this.state.author +
+          'coverPhotoUrl: ' + this.state.coverPhotoUrl +
+          'isbnNumber: ' + this.state.isbnNumber);
+    
+  }
+  
+  bookInfoChange(event) {
+    this.setState({
+      [event.target.name]:event.target.value
+    })
+  }
+  
+  
   render() {
     return (
-       <div className="text-white">
-         Add Book
-       </div>
+       <Card className={'border border-dark bg-dark text-white'}>
+         <Card.Header>Add Book</Card.Header>
+         <Form onSubmit={this.submitBook} id="bookFormId">
+           <Card.Body>
+             <Form.Row>
+               <Form.Group as={Col}>
+                 <Form.Label>Title</Form.Label>
+                 <Form.Control
+                    type="text"
+                    name="title"
+                    value={this.state.title}
+                    onChange={this.bookInfoChange}
+                    required
+                    className={'bg-dark text-white'}
+                    placeholder="Enter Book Title"/>
+               </Form.Group>
+               <Form.Group as={Col}>
+                 <Form.Label>Author</Form.Label>
+                 <Form.Control
+                    type="text"
+                    name="author"
+                    value={this.state.author}
+                    required
+                    onChange={this.bookInfoChange}
+                    className={'bg-dark text-white'}
+                    placeholder="Enter Author Name"/>
+               </Form.Group>
+             </Form.Row>
+             <Form.Row>
+               <Form.Group as={Col}>
+                 <Form.Label>Cover Photo URL</Form.Label>
+                 <Form.Control
+                    type="text"
+                    name="coverPhotoUrl"
+                    value={this.state.coverPhotoUrl}
+                    onChange={this.bookInfoChange}
+                    required
+                    className={'bg-dark text-white'}
+                    placeholder="Enter Book Cover Photo URL"/>
+               </Form.Group>
+               <Form.Group as={Col}>
+                 <Form.Label>ISBN Number</Form.Label>
+                 <Form.Control
+                    type="text"
+                    name="isbnNumber"
+                    value={this.state.isbnNumber}
+                    onChange={this.bookInfoChange}
+                    required
+                    className={'bg-dark text-white'}
+                    placeholder="Enter Book ISBN Number"/>
+               </Form.Group>
+             </Form.Row>
+           </Card.Body>
+           <Card.Footer style={{"textAlign":"right"}}>
+             <Button size={'sm'} variant="success" type="submit">
+               Submit
+             </Button>
+           </Card.Footer>
+         </Form>
+       </Card>
     );
   }
 }
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,31 @@
 import {Component} from 'react';
+import {Card, Table} from 'react-bootstrap';
 
 export default class BookList extends Component {
   render() {
     return (
-       <div className="text-white">
-         Book List
-       </div>
+       <Card className={"border border-dark bg-dark text-white"}>
+         <Card.Header>Book List</Card.Header>
+         <Card.Body>
+           <Table striped bordered hover variant="dark">
+             <thead>
+             <tr>
+               <th>Title</th>
+               <th>Author</th>
+               <th>ISBN Number</th>
+               <th>Price</th>
+               <th>Action</th>
+             </tr>
+             </thead>
+             <tbody>
+             
+             <tr align="center">
+               <td colSpan="6">No Book Available.</td>
+             </tr>
+             </tbody>
+           </Table>
+         </Card.Body>
+       </Card>
     );
   }
 }
