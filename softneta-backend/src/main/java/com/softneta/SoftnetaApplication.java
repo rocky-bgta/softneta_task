7d8cfb8acb0ea4d09f5a71bc4345dc8b56a60a9b
Salahin <salahin.dsi@bkash.com>
2021-05-25 14:35:05 +0600

    init back end project

---
+++ softneta-backend/src/main/java/com/softneta/SoftnetaApplication.java
@@ -0,0 +1,13 @@
+package com.softneta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftnetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftnetaApplication.class, args);
+	}
+
+}
---
+++ softneta-backend/src/test/java/com/softneta/SoftnetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softneta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftnetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
