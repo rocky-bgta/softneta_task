cd3651ba1e070c7fc570f04efb976fcd3586faeb
Salahin <salahin.dsi@bkash.com>
2021-05-26 22:34:58 +0600

    add delete functionality to PatientList.jsx

--- softneta-frontend/src/components/PatientList.jsx
+++ softneta-frontend/src/components/PatientList.jsx
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faEdit, faTrash,faList} from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import moment from 'moment';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default class PatientList extends Component {
   constructor(props) {
@@ -19,6 +20,9 @@ export default class PatientList extends Component {
     this.getPatientStudyList();
   }
   
+  successNotify = (message) => toast.success(message);
+  errorNotify = (message) => toast.error(message);
+  
   getPatientStudyList(){
     axios.get("http://localhost:3000/api/patient-info/list")
        .then(response=> response.data)
@@ -28,51 +32,69 @@ export default class PatientList extends Component {
        }).catch((response)=>{
          console.log(response);
     })
-    
   }
+  
+  deletePatientInfo = (patientId) => {
+    axios.delete("http://localhost:3000/api/patient-info/delete/"+patientId)
+       .then(response=> {
+         if(response.data!=null){
+           this.successNotify("Delete Patient successfully");
+           this.setState({
+             patientStudyList: this.state.patientStudyList.filter(patient => patient.id !== patientId)
+           })
+         }else {
+           this.errorNotify("Failed to delete patient");
+         }
+       })
+  }
+  
+  
   
   render() {
     return (
-       <Card className={'border border-dark bg-dark text-white'}>
-         <Card.Header> <FontAwesomeIcon icon={faList}/> Patient Study List</Card.Header>
-         <Card.Body>
-           <Table striped bordered hover variant="dark">
-             <thead>
-             <tr>
-               <th>Person Code</th>
-               <th>Patient Name</th>
-               <th>DOB</th>
-               <th>Study Name</th>
-               <th>Update Time</th>
-               <th>Action</th>
-             </tr>
-             </thead>
-             <tbody>
-             {
-               this.state.patientStudyList.length === 0 ?
-                  <tr align="center">
-                    <td colSpan="6">No Patient study recorded yet.</td>
-                  </tr> :
-                  this.state.patientStudyList.map((patientStudy,index) => (
-                     <tr key={patientStudy.id}>
-                       <td>{patientStudy.personCode}</td>
-                       <td>{patientStudy.fullName}</td>
-                       <td>{patientStudy.dob}</td>
-                       <td>{patientStudy.studyName}</td>
-                       <td>{moment(patientStudy.date).format('YYYY-MM-DD HH:mm:ss')}</td>
-                       <td>
-                         <ButtonGroup>
-                           <Button size={"sm"} variant={"outline-primary"} style={{margin: 6}}><FontAwesomeIcon icon={faEdit}/></Button>
-                           <Button size={"sm"} variant={"outline-danger"} style={{margin: 6}}><FontAwesomeIcon icon={faTrash}/></Button>
-                         </ButtonGroup>
-                       </td>
-                     </tr>
-                  ))
-             }
-             </tbody>
-           </Table>
-         </Card.Body>
-       </Card>
+       <div>
+         <ToastContainer autoClose={2000} />
+         <Card className={'border border-dark bg-dark text-white'}>
+           <Card.Header> <FontAwesomeIcon icon={faList}/> Patient Study List</Card.Header>
+           <Card.Body>
+             <Table striped bordered hover variant="dark">
+               <thead>
+               <tr>
+                 <th>Person Code</th>
+                 <th>Patient Name</th>
+                 <th>DOB</th>
+                 <th>Study Name</th>
+                 <th>Update Time</th>
+                 <th>Action</th>
+               </tr>
+               </thead>
+               <tbody>
+               {
+                 this.state.patientStudyList.length === 0 ?
+                    <tr align="center">
+                      <td colSpan="6">No Patient study recorded yet.</td>
+                    </tr> :
+                    this.state.patientStudyList.map((patientStudy,index) => (
+                       <tr key={index}>
+                         <td>{patientStudy.personCode}</td>
+                         <td>{patientStudy.fullName}</td>
+                         <td>{patientStudy.dob}</td>
+                         <td>{patientStudy.studyName}</td>
+                         <td>{moment(patientStudy.date).format('YYYY-MM-DD HH:mm:ss')}</td>
+                         <td>
+                           <ButtonGroup>
+                             <Button size={"sm"} variant={"outline-primary"} style={{margin: 6}}><FontAwesomeIcon icon={faEdit}/></Button>
+                             <Button size={"sm"} variant={"outline-danger"} onClick={this.deletePatientInfo.bind(this, patientStudy.id)} style={{margin: 6}}><FontAwesomeIcon icon={faTrash}/></Button>
+                           </ButtonGroup>
+                         </td>
+                       </tr>
+                    ))
+               }
+               </tbody>
+             </Table>
+           </Card.Body>
+         </Card>
+       </div>
     );
   }
 }
